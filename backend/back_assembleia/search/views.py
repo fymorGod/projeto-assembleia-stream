cda65ebf54a98165d83540b1a8f8e5cbdaf5eb65
José <ribeirojoseph44@gmail.com>
2022-02-01 11:27:22 -0300

    settings.py - correção da chave API youtube - v1.0.1

--- backend/back_assembleia/search/views.py
+++ backend/back_assembleia/search/views.py
@@ -1,5 +1,19 @@
+import requests
+
+from django.conf import settings
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    search_url = 'https://www.googleapis.com/youtube/v3/search'
+
+    params = {
+        'part':'snippet',
+        'q':'learn python',
+        'key': settings.YOUTUBE_DATA_API_KEY
+    }
+
+    r = requests.get(search_url, params=params)
+    print(r.text)
+
     return render(request, 'search/index.html')
