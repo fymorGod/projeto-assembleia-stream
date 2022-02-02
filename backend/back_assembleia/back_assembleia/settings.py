523df128aaeb52a9f9e207124fd9bfe600a004fd
José <ribeirojoseph44@gmail.com>
2022-02-02 13:50:41 -0300

    atualização projeto v1.0.0

--- backend/back_assembleia/back_assembleia/settings.py
+++ backend/back_assembleia/back_assembleia/settings.py
@@ -43,21 +43,23 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
 ]
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ORIGIN_WHITELIST = [
-    'https://localhost:3000'
-]
+# CORS_ORIGIN_WHITELIST = [
+#     # 'http://127.0.0.1:3000',
+#     # 'http://127.0.0.1:8000'
+#     'http://localhost:3000/'
+# ]
 
 ROOT_URLCONF = 'back_assembleia.urls'
 
