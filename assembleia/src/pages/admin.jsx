0761ac05e8e42ca49926a22cc6e696652103bad3
LuizFylip <54417643+fymorGod@users.noreply.github.com>
2022-02-02 15:41:03 -0300

    inicio page admin - construção base do sistema admin - v1.0.0

--- assembleia/src/pages/admin.jsx
+++ assembleia/src/pages/admin.jsx
@@ -1,7 +1,30 @@
-export function Admin(){
+//import { Navbar } from "../components/NavBar/Navbar";
+import { Navbar } from '../components/NavBar/Navbar'
+import '../styles/admin.css'
+export function Admin() {
     return (
-        <div>
-            admin 
+        <>
+        <Navbar />
+        <div className="container-admin-sistem">
+            <div className="container-forms">
+                <div className="box-menu">
+                    <div className="box-login">
+                        <h2 id='admin-user'>Admin User</h2>
+                        <p>Logout</p>
+                    </div>
+                    <div className="box-buttons">
+                    <button>Inserir vídeos</button>
+                    <button>Organizar</button>
+                    </div>
+                </div>
+                <div className="box-forms">
+                    <form>
+                        <input type="text" name='title-video' id='title-video' />
+                        <label htmlFor="title-video"></label>
+                    </form>
+                </div>
+            </div>
         </div>
+    </>
     )
 }
