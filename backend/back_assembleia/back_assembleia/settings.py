dc6717ae6c45b3b40b3ba50baac3f081462f02bd
José <ribeirojoseph44@gmail.com>
2022-02-02 14:00:53 -0300

    settings.py - att do CORS WHITELIST - v1.0.0

--- backend/back_assembleia/back_assembleia/settings.py
+++ backend/back_assembleia/back_assembleia/settings.py
@@ -53,13 +53,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 
-# CORS_ORIGIN_WHITELIST = [
-#     # 'http://127.0.0.1:3000',
-#     # 'http://127.0.0.1:8000'
-#     'http://localhost:3000/'
-# ]
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000'
+]
 
 ROOT_URLCONF = 'back_assembleia.urls'
 
