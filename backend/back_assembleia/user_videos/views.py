d24d06a287750cd4b524bbb37a71c3a1d2ac3bb7
José <ribeirojoseph44@gmail.com>
2022-02-04 15:32:28 -0300

    CRUD dos user_videos

--- backend/back_assembleia/user_videos/views.py
+++ backend/back_assembleia/user_videos/views.py
@@ -10,10 +10,10 @@ from .models import Video
 
 '''
 Funções:
-- (GET) video_detail: retorna um video com os detalhes [TO DO]
+- (GET) video_detail: retorna um video com os detalhes [DONE]
 - (POST) save_video: salva um vídeo no bd [DONE]
 - (UPDATE) edit_video: edita os dados de um vídeo [TO DO]
-- (DELETE) delete_video: deleta um vídeo do bd [TO DO]
+- (DELETE) delete_video: deleta um vídeo do bd [DONE]
 
 - (GET) videos_list: retorna uma lista com os vídeos do bd site [DONE] 
 '''
@@ -28,7 +28,7 @@ def videos_list(request):
 @api_view(['POST'])
 def save_video(request):
     if request.method == 'POST':
-        data_dicionario = (request.data).dict()
+        data_dicionario = request.data
 
         print(data_dicionario['file'])
         print(data_dicionario['description'])
@@ -36,14 +36,51 @@ def save_video(request):
         print(data_dicionario['file'])
 
         Video.objects.create(title=data_dicionario['file'], description=data_dicionario['description'], video=data_dicionario['file'], destaque=data_dicionario['destaque'])
+        return HttpResponse({'message':'Video inserido'}, status=200)
         
         # serializer = VideoSerializer(data=data_dicionario)
-        # print(serializer)
+        # print(serializer.data)
 
-        return HttpResponse({'message':'Video inserido'}, status=200)
 
         # if serializer.is_valid():
         #     print('\né valido')
         #     serializer.save()
         #     return Response(serializer.data, status=status.HTTP_201_CREATED)
         # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def delete_video(request, pk):
+
+    try:
+        video = Video.objects.get(pk=pk)
+    except Video.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'DELETE':
+        video.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET'])
+def video_detail(request, pk):
+
+    try:
+        video = Video.objects.get(pk=pk)
+    except Video.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':        
+        serializer = VideoSerializer(video, context={'request':request}, many=True)        
+        return Response(serializer.data)
+
+@api_view(['PUT'])
+def update_video(request, pk):
+
+    try:
+        video = Video.objects.get(pk=pk)
+    except Video.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'PUT':
+        serializer = VideoSerializer(video, data=request.data, context={'request':request}, many=True)
+        Video.objects.update(data=serializer.data)
+        return Response(status=status.HTTP_201_CREATED)
