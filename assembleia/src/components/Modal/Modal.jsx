523835c60f0211a9252b33cf08d3be507319aeac
LuizFylip <54417643+fymorGod@users.noreply.github.com>
2022-02-07 13:38:19 -0300

    atualizaçao layout

--- assembleia/src/components/Modal/Modal.jsx
+++ assembleia/src/components/Modal/Modal.jsx
@@ -1,4 +1,15 @@
+import { useEffect, useState } from "react";
+import { api } from "../../api/app";
+
 export function Modal({closeModal}){
+    const [videos, setVideo] = useState([]);
+    
+    useEffect(() => {
+        api.get('/search/').then(response => {
+            console.log(response.data[0].title);
+            setVideo(response.data);
+        })
+    }, [])
     return(
         <div className="modal">
             <div className="modalContainer">
