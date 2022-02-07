b0a7ddd01bbd6a320a6c59fdcb6c076450ce02b6
LuizFylip <54417643+fymorGod@users.noreply.github.com>
2022-02-07 15:05:27 -0300

    montando front admin - criando processo de selecionar os videos - v1.0.0

--- assembleia/src/App.jsx
+++ assembleia/src/App.jsx
@@ -3,6 +3,7 @@ import { Login } from "./pages/login";
 import { BrowserRouter,Routes, Route} from 'react-router-dom';
 import { Admin } from "./pages/admin";
 import { Register } from "./pages/register";
+import { Organizar } from "./pages/organizar";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/admin/login' element={<Login />} />
         <Route path='/admin/system' element={<Admin />} />
         <Route path='/admin/register' element={<Register />} />
+        <Route path='/admin/system/organizar' element={<Organizar />} />
       </Routes>
     </BrowserRouter>
   );
