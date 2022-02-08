d490748e975d67468eb33c9ea438918ce1423dad
José <ribeirojoseph44@gmail.com>
2022-02-08 15:43:56 -0300

    sei la mano

--- backend/back_assembleia/auntt/apps.py
+++ backend/back_assembleia/auntt/apps.py
@@ -1,12 +1,5 @@
 from django.apps import AppConfig
 
-
-<<<<<<<< HEAD:backend/back_assembleia/app_searchBar/apps.py
-class AppSearchbarConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_searchBar'
-========
 class AunttConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'auntt'
->>>>>>>> RM3:backend/back_assembleia/auntt/apps.py
