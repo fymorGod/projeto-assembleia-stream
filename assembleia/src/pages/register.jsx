eed5500ab03cfa50fe5d347cffa44adbacf9b691
LuizFylip <54417643+fymorGod@users.noreply.github.com>
2022-02-08 16:32:10 -0300

    auth em processo - v1.0.2

--- assembleia/src/pages/register.jsx
+++ assembleia/src/pages/register.jsx
@@ -16,7 +16,7 @@ export function Register() {
     // }, [])
 
     function handleCreateUser() {
-        api.get('admin/login/submit/', {
+        api.get('/register/', {
             username: username,
             password: password
         }).then(response => {
