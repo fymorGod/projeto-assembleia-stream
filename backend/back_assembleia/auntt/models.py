511106494ff34fb9dffc60d08d90c7cb930ff3f9
Ramon Oliveira <ramon.oliveira@mirante.com.br>
2022-02-08 16:59:38 -0300

    colocando email do banco

--- backend/back_assembleia/auntt/models.py
+++ backend/back_assembleia/auntt/models.py
@@ -7,8 +7,8 @@ class User(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
     createdAt = models.DateTimeField(auto_now_add=True)
-    
+    email = models.EmailField(default="")
 
     def __str__(self):
         return self.username
-
+   
--- backend/back_assembleia/auntt/views.py
+++ backend/back_assembleia/auntt/views.py
@@ -28,6 +28,7 @@ def create_user(request):
         print(serializer.errors)
         if serializer.is_valid():
             serializer.save()
+            print(serializer.data)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
